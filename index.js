61885a68fa9aef8ba769b76725c05e5d061ecec0
lexus2727 <lexus2424@yahoo.com>
2019-12-24 20:41:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = 0;
+
+multiply = num1 * num2;
+
+let random = 0;
+
+random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 0;
+let num3 = 9;
+let num4 = 5;
+
+mod = num3 % num4;
+
+let max = 0;
+
+max = Math.max(5, 10, 15, 20);
+
+
